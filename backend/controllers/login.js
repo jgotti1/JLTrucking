0cd3e15643aa950ad91f4f557991178f80e78afb
Margotti <margottij@win.dowjones.net>
2024-05-08 10:56:08 -0400

    finished login controller setup

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -51,38 +51,3 @@ module.exports = router;
 
 
 
-const checkUser = async (req, res) => {
-  const { username, userPassword} = req.body;
-  // console.log("Username:", username); // Log the username provided by the user
-
-  try {
-    // Retrieve the hashed password from the database for the provided username
-    const query = "SELECT password_hash, isAdmin FROM login WHERE username = $1";
-    const result = await pool.query(query, [username]);
-    // console.log("Query Result:", result.rows); // Log the result of the database query
-
-    if (result.rows.length === 0) {
-      // Username not found
-      return res.status(404).json({ message: "Username not found" });
-    }
-
-    const hashedPassword = result.rows[0].password_hash;
-    const isAdminResult = result.rows[0].isadmin
-   
-    // console.log("Hashed Password:", hashedPassword); // Log the hashed password retrieved from the database
-
-    // Compare the provided password with the hashed password retrieved from the database
-    const passwordMatch = await bcrypt.compare(userPassword, hashedPassword);
-
-    if (passwordMatch) {
-      // Passwords match
-      res.status(200).json({ message: "Username and password match", isAdminResult });
-    } else {
-      // Passwords do not match
-      res.status(401).json({ message: "Username and password do not match" });
-    }
-  } catch (error) {
-    console.error("Error checking user:", error.message); // Log any error that occurs
-    res.status(500).json({ message: "Error checking user" });
-  }
-};
